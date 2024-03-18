41a04d7f799ac7f339bac69f06b305c48fb01359
Diego Saavedra <dsaavedra88@gmail.com>
2024-03-18 07:41:26 -0500

    Update urls.py
    Explicación del error

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -5,3 +5,5 @@ urlpatterns = [
     # define la ruta para la vista hello_world.
     path('',hello_world) #cuando se teclee hello/ en el navegador se llama a la funcion hello_world
 ]
+
+# Como se indica en la línea 6 en el comentario, la ruta debe ser 'hello/' y no ''
